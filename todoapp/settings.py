c4b1cd66324b59315d69dd6fdd9d31fff030d8c7
Biegon <156933863+Bigoncode@users.noreply.github.com>
2024-02-24 07:11:52 +0300

    deployment bugs solved

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bak1g6nzuvz!(sntd-k$^%*4$yzny(6b_dggsbbn0lx3s$_@e@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
